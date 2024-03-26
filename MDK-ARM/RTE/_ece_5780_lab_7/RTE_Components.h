27eb4e92b98e23cf9ddc62b5023571cbc56a909d
Aidan Case <aidan.case@casefamily.us>
2024-03-26 08:11:15 -0600

    initial commit

---
+++ MDK-ARM/RTE/_ece_5780_lab_7/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ece_5780_lab_7' 
+ * Target:  'ece_5780_lab_7' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
